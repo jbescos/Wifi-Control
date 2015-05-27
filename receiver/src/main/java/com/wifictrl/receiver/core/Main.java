cc912f30e1aafd22d46b4939cc67aa5294a3561d
jbescos <jorgebescos@gmail.com>
2015-05-27 22:42:12 +0200

    Doing a complete test

--- receiver/src/main/java/com/wifictrl/receiver/core/Main.java
+++ receiver/src/main/java/com/wifictrl/receiver/core/Main.java
@@ -3,8 +3,8 @@ package com.wifictrl.receiver.core;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Thread thread = new Thread(new Receiver(new BotHandler()));
+		thread.start();
 	}
 
 }
---
+++ receiver/src/test/java/com/wifictrl/receiver/core/BotHandlerTest.java
@@ -0,0 +1,75 @@
+package com.wifictrl.receiver.core;
+
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import com.wifictrl.common.core.Constants;
+import com.wifictrl.common.core.Info;
+import com.wifictrl.sender.core.DatagramManager;
+
+public class BotHandlerTest {
+
+	@Mock
+	private Handler handler;
+	private final ObjectMapper mapper = new ObjectMapper();
+	private List<Info<?>> dataSample;
+	private final Logger log = LogManager.getLogger();
+	
+	@Before
+	public void before(){
+		MockitoAnnotations.initMocks(this);
+		dataSample = new ArrayList<Info<?>>();
+		dataSample.add(new Info<>());
+		dataSample.add(new Info<>());
+	}
+	
+	@Test
+	public void sample() throws IOException, InterruptedException{
+		final byte[] data = new byte[Constants.PACKET_SIZE];
+		final byte[] src = mapper.writeValueAsBytes(dataSample);
+		System.arraycopy(src, 0, data, 0, src.length);
+		final int ACTIONS = 10;
+		final CountDownLatch actions = new CountDownLatch(ACTIONS);
+		doAnswer(new Answer<Void>() {
+			@Override
+			public Void answer(InvocationOnMock invocation) throws Throwable {
+				byte[] received = (byte[]) invocation.getArguments()[0];
+				Arrays.equals(data, received);
+				List<Info<?>> fromJson = mapper.readValue(received, new TypeReference<List<Info<?>>>() {});
+				Arrays.equals(dataSample.toArray(new Info<?>[0]), fromJson.toArray(new Info<?>[0]));
+				actions.countDown();
+				log.debug("Received");
+				return null;
+			}
+		}).when(handler).handle((byte[]) anyObject());
+		new Thread(new Receiver(handler)).start();
+		try(DatagramManager manager = new DatagramManager();){
+			for(int i=0;i<ACTIONS;i++){
+				manager.send(data);
+				log.debug("Sended");
+			}
+		}
+		actions.await();
+		verify(handler, times(ACTIONS)).handle(data);
+	}
+	
+}
