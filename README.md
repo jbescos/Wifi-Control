5948557466c490395628af2e0b3bd52b52539f13
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2024-01-10 15:56:46 +0100

    JDK 11 and improvements
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Wifi-Control
 Use the keyboard and mouse of one computer to control other
+
+##For receiver:
+
+export DISPLAY=:0
+
+java -jar -Djava.awt.headless=false receiver/target/receiver-1.1-jar-with-dependencies.jar
+
+##For sender
+
+java -jar sender/target/sender-1.1-jar-with-dependencies.jar {REMOTE_IP} 9876
--- receiver/src/test/java/com/wifictrl/receiver/core/BotHandlerTest.java
+++ receiver/src/test/java/com/wifictrl/receiver/core/BotHandlerTest.java
@@ -1,7 +1,6 @@
 package com.wifictrl.receiver.core;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.anyObject;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -26,47 +25,47 @@ import com.wifictrl.sender.core.DatagramManager;
 
 public class BotHandlerTest {
 
-	@Mock
-	private Handler handler;
-	private final Logger log = LogManager.getLogger();
-	
-	@Before
-	public void before(){
-		MockitoAnnotations.initMocks(this);
-	}
-	
-	@Test
-	public void sample() throws IOException, InterruptedException, ClassNotFoundException{
-		Info<Integer[]> i1 = new Info<>();
-		i1.setAction(1);
-		i1.setData(new Integer[]{45,78,564,7876,334});
-		final byte[] data = new byte[Constants.PACKET_SIZE];
-		final byte[] src = SerializeData.toBytes(i1);
-		System.arraycopy(src, 0, data, 0, src.length);
-		final int ACTIONS = 10;
-		final CountDownLatch actions = new CountDownLatch(ACTIONS);
-		doAnswer(new Answer<Void>() {
-			@Override
-			public Void answer(InvocationOnMock invocation) throws Throwable {
-				try{
-					byte[] received = (byte[]) invocation.getArguments()[0];
-					log.debug("Received: "+Arrays.toString(received));
-					assertEquals(Arrays.toString(data), Arrays.toString(received));
-				}finally{
-					actions.countDown();
-				}
-				return null;
-			}
-		}).when(handler).handle((byte[]) anyObject());
-		new Thread(new Receiver(handler), "test-thread").start();
-		try(DatagramManager manager = new DatagramManager("localhost");){
-			for(int i=0;i<ACTIONS;i++){
-				manager.send(data);
-				log.debug("Sended");
-			}
-		}
-		actions.await();
-		verify(handler, times(ACTIONS)).handle(data);
-	}
-	
+    @Mock
+    private Handler handler;
+    private final Logger log = LogManager.getLogger();
+
+    @Before
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void sample() throws IOException, InterruptedException, ClassNotFoundException {
+        Info<Integer[]> i1 = new Info<>();
+        i1.setAction(1);
+        i1.setData(new Integer[] { 45, 78, 564, 7876, 334 });
+        final byte[] data = new byte[Constants.PACKET_SIZE];
+        final byte[] src = SerializeData.toBytes(i1);
+        System.arraycopy(src, 0, data, 0, src.length);
+        final int ACTIONS = 10;
+        final CountDownLatch actions = new CountDownLatch(ACTIONS);
+        doAnswer(new Answer<Void>() {
+            @Override
+            public Void answer(InvocationOnMock invocation) throws Throwable {
+                try {
+                    byte[] received = (byte[]) invocation.getArguments()[0];
+                    log.debug("Received: " + Arrays.toString(received));
+                    assertEquals(Arrays.toString(data), Arrays.toString(received));
+                } finally {
+                    actions.countDown();
+                }
+                return null;
+            }
+        }).when(handler).handle(data);
+        new Thread(new Receiver(handler), "test-thread").start();
+        try (DatagramManager manager = new DatagramManager("localhost");) {
+            for (int i = 0; i < ACTIONS; i++) {
+                manager.send(data);
+                log.debug("Sended");
+            }
+        }
+        actions.await();
+        verify(handler, times(ACTIONS)).handle(data);
+    }
+
 }
